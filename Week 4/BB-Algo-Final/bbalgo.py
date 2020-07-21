312119d33e41e23651392b381bcfb3b3c440c09f
lambdamirror <lambdamirror@users.noreply.github.com>
2020-07-21 15:48:11 +1000

    Update

---
+++ Week 4/BB-Algo-Final/bbalgo.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 23:16:07 2020
+
+@author: tranl
+"""
+import time, sys, os
+import numpy as np
+import pandas as pd
+
+from binancepy import MarketData, Client
+from ultilities import print_, barstr, timestr
+from tradingpy import TradingModel, Portfolio
+from wss import wss_run
+
+## Model setups
+min_in_candle = 1
+pd_ob = int(30/min_in_candle)
+pd_es = 15
+
+def main(args):
+    start_time = time.time()
+    testnet = True
+    filename = str(int(time.time()))
+    if testnet:
+        # Testnet
+        apikey = '' ### INSERT your api key here ###
+        scrkey = '' ### INSERT your api secret here ###
+    else:
+        # Binance
+        apikey = ''
+        scrkey = ''
+
+    if testnet: fileout =  "report/testnet-" + filename
+    else: fileout =  "report/" + filename
+
+    insIds = [ 'BTCUSDT', 'ETHUSDT', 'BCHUSDT', 'BNBUSDT' ]
+    
+    # Generate Client object
+    client = Client(apikey, scrkey, testnet=testnet)
+    client.change_position_mode(dualSide='true')
+    
+    # Generate Portfolio object
+    portfolio = Portfolio(client, tradeIns=insIds)
+    long, short = portfolio.equity_distribution(longPct=0.25, shortPct=0.25, currency='USDT', orderPct=0.05)
+    portfolio.position_locks()
+
+    print_('\n' + barstr('', length=100, space_size=0), fileout)
+    print_(barstr('BINANCE TRADING', length=100, space_size=5), fileout)
+    print_(barstr('', length=100, space_size=0) + '\n', fileout)
+
+    print_('\n' + barstr('Generating Models', length=100, space_size=5) + '\n', fileout)
+    # Generate Models object
+    models = {}
+    for i in range(len(portfolio.tradeIns)):
+        symbol = portfolio.tradeIns[i]
+        client.change_leverage(symbol, 1)
+        _data = MarketData(testnet=testnet, symbol=symbol)
+        model = TradingModel(symbol=symbol, testnet=testnet, modelType='bollinger', marketData=_data, pdObserve=pd_ob, pdEstimate=pd_es, orderSize=portfolio.orderSize)
+        model.build_initial_input()
+        only_pos = 'BOTH'
+        if symbol in portfolio.locks['BUY']:
+            model.add_signal_lock(slock='BUY')
+            only_pos = 'SELL ONLY'
+        elif symbol in portfolio.locks['SELL']:
+            model.add_signal_lock(slock='SELL')
+            only_pos = 'BUY ONLY'
+        models[symbol] = model
+        print_('\tFinish generating model for %s - positions: %s' % (symbol, only_pos), fileout)
+
+    print_('\n' + barstr('Start Data Streaming', length=100, space_size=5) + '\n', fileout)
+    header_print(testnet, client, portfolio, fileout)
+    print('\n\tPre-processing Time = %f' % (time.time() - start_time))
+
+    print_('\nStream updating for {} minutes...'.format(pd_ob*min_in_candle), fileout)
+    signals = wss_run(portfolio, client, testnet, ['@kline_1m'], models, fileout)
+
+    session_summary(signals, fileout)
+    print_('\n\tLocal Time at Close: %s ' % timestr(time.time()*1000), fileout)
+    
+    print_(barstr(text='Elapsed time = {} seconds'.format(round(time.time()-start_time,2))), fileout)
+    print_(barstr(text="", space_size=0), fileout)
+    os._exit(1)
+
+def header_print(testnet, client, portfolio, file):
+    '''
+    Print general information of the trading session
+    '''
+    t_server, t_local = client.timestamp(), time.time()*1000
+    print_('\tTestnet: %s' % str(testnet), file)
+    print_('\tServer Time at Start: %s' % timestr(t_server), file)
+    print_('\tLocal Time at Start: %s, \tOffset (local-server): %d ms\n' % (timestr(t_local), (t_local-t_server)), file)
+    print_('\t#LONG order : %d \t#SHORT order: %d \tOrder Size : %1.2f \n' % (portfolio.equityDist['BUY'], portfolio.equityDist['SELL'], portfolio.orderSize), file)
+    try:
+        bal_st = pd.DataFrame(client.balance())
+        bal_st['updateTime'] = [timestr(b) for b in bal_st['updateTime']]
+        print_('\nBeginning Balance Info: \n', file)
+        print_(bal_st, file)
+    except Exception:
+         print_('\nFail to connect to client.balance: \n', file)
+    
+def session_summary(signals, file):
+    '''
+    Print summary statistics of the trading session
+    '''
+    gross_profit = 0
+    gross_loss = 0
+    win, loss, inpos = 0, 0, 0
+    exp_signal = 0
+    trade_time = 0
+    for symbol in signals:
+        for sig in signals[symbol]:
+            if sig.is_expired(): exp_signal += 1
+            elif sig.is_closed():           
+                if sig.clsPrice is not None:
+                    if sig.side=='BUY': _side = 1.0
+                    elif sig.side=='SELL': _side = -1.0
+                    pnl = _side*sig.get_quantity()*(sig.clsPrice - sig.excPrice)
+                    trade_time += (sig.clsTime - sig.excTime)/(60*1000)
+                    if pnl > 0: 
+                        win += 1
+                        gross_profit += pnl
+                    if pnl <= 0:
+                        loss += 1
+                        gross_loss += pnl
+                else:
+                    inpos += 1
+    if (win+loss) > 0: timeAv = trade_time/(win+loss)
+    else: timeAv = 0.0
+    print_( '\n####################\tTrading Session Summary\t####################\n', file)
+    print_( '\n\tGross Profit: \t%1.5f' % gross_profit, file)
+    print_( '\tGross Loss: \t%1.5f' % gross_loss, file)
+    print_( '\tNet Profit: \t%1.5f' % (gross_profit+gross_loss), file)
+    print_( '\tAverage time in position: \t%1.2f' % timeAv, file)
+    print_( '\tNumber of win trades: \t%d' % win, file)
+    print_( '\tNumber of loss trades: \t%d' % loss, file)
+    print_( '\tNumber of unfinished trades: \t%d' % inpos, file)
+    print_( '\tNumber of expired signals: \t%d \n' % exp_signal, file)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+      
---
+++ Week 4/BB-Algo-Final/indicators.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 24 00:16:07 2020
+
+@author: tranl
+"""
+import numpy as np
+import pandas as pd
+        
+def OBVol(df):
+    '''
+    Returns on-balance volume
+    '''
+    _obv = np.zeros(df.shape[0])
+    _obv[0] = df['_v'].iloc[0]
+    for i in range(1, df.shape[0]):
+        if df['_c'].iloc[i] > df['_c'].iloc[i-1]:
+            _obv[i] = _obv[i-1] + df['_v'].iloc[i]
+        elif df['_c'].iloc[i] == df['_c'].iloc[i-1]:
+            _obv[i] = _obv[i-1] 
+        elif df['_c'].iloc[i] < df['_c'].iloc[i-1]:     
+            _obv[i] = _obv[i-1] - df['_v'].iloc[i]
+    return _obv
+      
+def MACD(df, fpd=12, spd=26):
+    '''
+    Returns moving average convergence/divergence
+    '''
+    fastema = df['_c'].ewm(span=fpd).mean()
+    slowema = df['_c'].ewm(span=spd).mean()
+    _macd = fastema - slowema
+    return _macd
+
+def Williams(df, period=14):
+    """ returns Williams indicator """
+    low = df['_l'].rolling(period).min()
+    high = df['_h'].rolling(period).max()
+    _will = -100*(high-df['_c'])/(high-low)
+    return _will
+
+def StochOsc(df, period=14):
+    '''
+    Returns stochastic oscilatior indicator
+    '''
+    low = df['_l'].rolling(period).min()
+    high = df['_h'].rolling(period).max()
+    _stoch = 100*(df['_c']-low)/(high-low)
+    return _stoch
+
+def RSIfunc(df, period=14):
+    '''
+    Returns RSI values
+    '''
+    diff = df['_c'].diff().dropna()
+    first = diff.iloc[:period]
+    _rsi = np.zeros(df['_c'].shape[0])
+    _rsi[:period] = np.nan
+    gain = np.zeros(df['_c'].shape[0] - period)
+    loss = np.zeros(df['_c'].shape[0] - period)
+    gain[0] = abs(first[first>0].sum())
+    loss[0] = abs(first[first<0].sum())    
+    for i in range(1, len(gain)):
+        change = diff.iloc[period+i-1]
+        gain[i] = gain[i-1]*(period-1)/period + abs(change*int(change>0))
+        loss[i] = loss[i-1]*(period-1)/period + abs(change*int(change<0))
+    for i in range(gain.shape[0]):
+        if loss[i] == 0:
+            _rsi[i+period] = 100
+        else:
+            _rsi[i+period] = 100 - 100/(1+gain[i]/loss[i])
+    return _rsi
+
+def Bbands(df, window=None, width=None, numsd=None):
+    '''
+    Returns average, upper band, and lower band
+    '''
+    ave = df.rolling(window).mean()
+    sd = df.rolling(window).std(ddof=0)
+    if width:
+        upband = ave * (1+width)
+        dnband = ave * (1-width)
+        return ave, upband, dnband        
+    if numsd:
+        upband = ave + (sd*numsd)
+        dnband = ave - (sd*numsd)
+        return ave, upband, dnband   
+        
+def average_true_range(df, period=10, alpha=0.5, highlow=True):   
+    '''
+    Returns average true range at quantile alpha and percentage on average mid point
+    '''
+    _tr = df.copy()
+    _tr['h-l'] = _tr['_h'].rolling(period).max() - _tr['_l'].rolling(period).min()
+    _tr['h-o'] = (_tr['_h'].rolling(period).max() - _tr['_c'].shift(period)).abs()
+    _tr['l-o'] = (_tr['_l'].rolling(period).min() - _tr['_c'].shift(period)).abs()
+    if highlow:
+        atr = pd.concat([_tr['h-l'], _tr['h-o'], _tr['l-o']], axis=1).dropna().max(axis=1).quantile(alpha)
+    else:
+        atr = pd.concat([_tr['h-o'], _tr['l-o']], axis=1).dropna().max(axis=1).quantile(alpha)
+    atr_pct = atr/((_tr['_h'] + _tr['_l'])/2).mean()
+    return atr, atr_pct
